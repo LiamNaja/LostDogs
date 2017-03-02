e47761d72dc249d8d73b6c3174848dcd119cdff6
boom <boom14454@gmail.com>
2017-03-02 11:00:55 +0700

    Create SignUp OK

---
+++ app/src/main/java/drusp/rathapon/burin/lostdogs/SignUp.java
@@ -0,0 +1,13 @@
+package drusp.rathapon.burin.lostdogs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
